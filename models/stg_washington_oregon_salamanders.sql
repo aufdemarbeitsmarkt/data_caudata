a44fd6e52f2a71f030bdc200305cbb87a2c7fc4b
Tim <37612712+aufdemarbeitsmarkt@users.noreply.github.com>
2023-01-06 09:39:44 -0800

    add initial stg model

--- models/stg_washington_oregon_salamanders.sql
+++ models/stg_washington_oregon_salamanders.sql
@@ -5,7 +5,30 @@
 }}
 
 with base as (
-    SELECT *
+    SELECT 
+         inaturalist_id,
+	     taxon_id,
+	     name as latin_name,
+	     preferred_common_name,
+	     observed_date,
+	     observed_month::INTEGER as observed_month,
+	     observed_hour::INTEGER as observed_hour,
+	     observed_week::INTEGER as observed_week,
+	     observed_year::INTEGER as observed_year,
+	     observed_day::INTEGER as observed_day,
+	     species_guess,
+	     (species_guess = preferred_common_name) as      species_guess_correct,
+	     identifications_most_disagree,
+	     place_ids::INTEGER[] as place_id_array,
+	     location as coordinates,
+         SPLIT_PART(location, ',', 1)::DOUBLE PRECISION as latitude,
+	     SPLIT_PART(location, ',', 2)::DOUBLE PRECISION as longitude,
+	     endemic,
+	     native,
+	     introduced,
+	     threatened,
+	     rank,
+	     wikipedia_url
     FROM {{source('datacaudata', 'washington_oregon_salamanders')}}
 )
 
